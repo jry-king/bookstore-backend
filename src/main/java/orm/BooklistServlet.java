242e7d30a5e99720b7f8733777ba8cfc868dc099
jry-king <2472559327@qq.com>
2018-05-02 21:15:10 +0800

    ？？？

--- src/main/java/orm/BooklistServlet.java
+++ src/main/java/orm/BooklistServlet.java
@@ -2,6 +2,7 @@ package orm;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,6 +11,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  * Servlet implementation class UserManagerServlet
@@ -35,18 +39,29 @@ public class BooklistServlet extends HttpServlet {
             HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
             // Write HTML header
             PrintWriter out = response.getWriter();
-            response.setContentType("text/html;charset=utf-8");
-            out.println("<html><head><title>User Manager</title></head><body>");
+            response.setContentType("application/json;charset=utf-8");
+            response.setCharacterEncoding("UTF-8");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
 
-           // Print page
-           //printUserForm(out);
-           listBook(out);
-
-           // Write HTML footer
-           out.println("</body></html>");
-           out.flush();
-           out.close();
-           HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+            @SuppressWarnings({ "unchecked" })
+            List<Book> result = HibernateUtil.getSessionFactory()
+                   .getCurrentSession().createQuery("from Book").list();
+            Iterator<Book> it = result.iterator();
+            JSONArray booksJson = new JSONArray();
+            JSONObject res = null;
+            while (it.hasNext()) {
+                Book book = (Book) it.next();
+                JSONObject bookjson = JSONObject.fromObject(book);
+                booksJson.add(bookjson);
+                res = JSONObject.fromObject(book);
+            }
+            out.print(res);
+            out.flush();
+            out.close();
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
         }
         catch (Exception ex) {
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
@@ -58,44 +73,5 @@ public class BooklistServlet extends HttpServlet {
             }
         }
 	}
-
-	private void printUserForm(PrintWriter out) {
-        out.println("<h2>Add new user:</h2>");
-        out.println("<form>");
-        out.println("Title: <input name='eventTitle' length='50'/><br/>");
-        out.println("Date (e.g. 24.12.2009): <input name='eventDate' length='10'/><br/>");
-        out.println("<input type='submit' name='action' value='store'/>");
-        out.println("</form>");
-    }
-	
-	@SuppressWarnings({ "unchecked" })
-	private void listBook(PrintWriter out) {
-
-        List<Book> result = HibernateUtil.getSessionFactory()
-                .getCurrentSession().createQuery("from Book").list();
-        if (result.size() > 0) {
-            out.println("<h2>Users in database:</h2>");
-            out.println("<table border='1'>");
-            out.println("<tr>");
-            out.println("<th>id</th>");
-            out.println("<th>bookname</th>");
-            out.println("<th>author</th>");
-            out.println("<th>language</th>");
-            out.println("<th>published</th>");
-            out.println("</tr>");
-            Iterator it = result.iterator();
-            while (it.hasNext()) {
-                Book user = (Book) it.next();
-                out.println("<tr>");
-                out.println("<td>" + user.getId() + "</td>");
-                out.println("<td>" + user.getBookname() + "</td>");
-                out.println("<td>" + user.getAuthor() + "</td>");
-                out.println("<td>" + user.getLanguage() + "</td>");
-                out.println("<td>" + user.getPublished() + "</td>");
-                out.println("</tr>");
-            }
-            out.println("</table>");
-        }
-    }
 
 }
